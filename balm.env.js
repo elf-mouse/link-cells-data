ed7dd190e42e94279667b0ebbe2ca3dc94ca2863
elf-mouse <ifmouz@gmail.com>
2025-07-01 17:20:52 +0800

    chore: add balm-git-flow config

---
+++ balm.env.js
@@ -0,0 +1,5 @@
+process.env.BALM_GIT_FLOW_MAIN = "dev";
+process.env.BALM_GIT_FLOW_RELEASE = "release";
+process.env.BALM_GIT_FLOW_RELEASES = ["release"];
+process.env.BALM_GIT_FLOW_SCRIPTS = ["prod"];
+process.env.BALM_GIT_FLOW_BUILD_DIR = "dist";
